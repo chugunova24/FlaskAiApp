a370215258cbfb4a278175b3fa5450285f46af55
Танюха <80966519+chugunova24@users.noreply.github.com>
2021-11-26 22:36:23 +0300

    Добавила фон с сабекой, который при каждом обновлении меняется

--- app.py
+++ app.py
@@ -1,79 +1,66 @@
-# Importing the required libraries
+from flask import Flask, render_template, request
 import requests
 import os
 import uuid
 import json
 from dotenv import load_dotenv
-load_dotenv()
-
-from flask import Flask, render_template, request
+from PIL import Image
 
 app = Flask(__name__)
-
+load_dotenv()
 
 @app.route('/', methods=['GET'])
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 
-# code
 @app.route('/', methods=['POST'])
 def index_post():
-	# Read the values from the form
-	original_text = request.form['text']
-	target_language = request.form['language']
+    # Read the values from the form
+    original_text = request.form['text']
+    target_language = request.form['language']
 
-	# Load the values from .env
-	key = os.environ['KEY']
-	endpoint = os.environ['ENDPOINT']
-	location = os.environ['LOCATION']
+    # Load the values from .env
+    key = os.environ['KEY']
+    endpoint = os.environ['ENDPOINT']
+    location = os.environ['LOCATION']
 
-	# Indicate that we want to translate and the API
-	# version (3.0) and the target language
-	path = '/translate?api-version=3.0'
+    # Indicate that we want to translate and the API
+    # version (3.0) and the target language
+    path = '/translate?api-version=3.0'
 
-	# Add the target language parameter
-	target_language_parameter = '&to=' + target_language
+    # Add the target language parameter
+    target_language_parameter = '&to=' + target_language
 
-	# Create the full URL
-	constructed_url = endpoint + path + target_language_parameter
+    # Create the full URL
+    constructed_url = endpoint + path + target_language_parameter
 
-	# Set up the header information, which includes our
-	# subscription key
-	headers = {
-		'Ocp-Apim-Subscription-Key': key,
-		'Ocp-Apim-Subscription-Region': location,
-		'Content-type': 'application/json',
-		'X-ClientTraceId': str(uuid.uuid4())
-	}
+    # Set up the header information, which includes our
+    # subscription key
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
 
-	# Create the body of the request with the text to be
-	# translated
-	body = [{'text': original_text}]
+    # Create the body of the request with the text to be
+    # translated
+    body = [{'text': original_text}]
 
-	# Make the call using post
-	translator_request = requests.post(
-		constructed_url, headers=headers, json=body)
+    # Make the call using post
+    translator_request = requests.post(
+        constructed_url, headers=headers, json=body)
 
-	# Retrieve the JSON response
-	translator_response = translator_request.json()
+    # Retrieve the JSON response
+    translator_response = translator_request.json()
 
-	# Retrieve the translation
+    # Retrieve the translation
+    translated_text = translator_response[0]['translations'][0]['text']
 
-	# translated_text = translator_response[0]['translations'][0]['text']
-
-
-	# Call render template, passing the translated text,
-	# original text, and target language to the template
-	return render_template(
-		'index.html',
-		translated_text=translator_response,
-		original_text=original_text,
-		target_language=target_language)
-
-
-@app.route('/', methods=['GET','POST'])
-def test():
-     if request.method == "POST":
-          translator_response=request.json['data']
-     return render_template('index.html')
+    return render_template(
+        'index.html',
+        translated_text=translated_text,
+        original_text=original_text,
+        target_language=target_language
+    )
